2867057b5ef1103378838f93e8d4b4f0c4d6952e
omarashrafdev <omar.ashraf.dev@gmail.com>
2023-12-13 18:42:37 +0200

    Home page draft

--- website/src/pages/Home.jsx
+++ website/src/pages/Home.jsx
@@ -1,42 +1,73 @@
-import { Container, Heading, Stack, Text, Button } from '@chakra-ui/react';
+import {
+  Container,
+  Image,
+  Stack,
+  Box,
+  Text,
+  Flex,
+  IconButton,
+  Link,
+} from "@chakra-ui/react";
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaYoutube,
+} from "react-icons/fa"; // Import social icons
 
 export function Home() {
   return (
-    <Container maxW={'5xl'}>
+    <Container>
       <Stack
-        textAlign={'center'}
-        align={'center'}
+        textAlign={"center"}
+        align={"center"}
         spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}>
-        <Heading
-          fontWeight={600}
-          fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-          lineHeight={'110%'}>
-          Finding Places{' '}
-          <Text as={'span'} color={'blue.400'}>
-            made easy
-          </Text>
-        </Heading>
-        <Text color={'gray.500'} maxW={'3xl'}>
-          Discover a world of possibilities with{' '}
-          <Text as="span" color={'blue.400'} fontWeight={'bold'}>
-            Inplace
-          </Text>{' '}
-          and join the community that&apos;s transforming the way we rent and share rooms.
-        </Text>
-        <Stack spacing={6} direction={'row'}>
-          <Button
-            rounded={'full'}
-            px={6}
-            colorScheme={'blue'}
-            bg={'blue.400'}
-            _hover={{ bg: 'blue.500' }}>
-            Get started
-          </Button>
-          <Button rounded={'full'} px={6}>
-            Learn more
-          </Button>
-        </Stack>
+      >
+        <Box
+          bgColor={"blue.700"}
+          w={"100vw"}
+          minH={"50vh"}
+          py={20}
+          color="white"
+        >
+          <Flex direction={{ base: "column", md: "row" }} align="center">
+            {/* Left side with logo and description */}
+            <Flex direction="column" flex="1" px={8}>
+              <Box maxH="200px" mb={6}>
+                <Image
+                  src="/logo-text.png"
+                  boxSize="100%"
+                  objectFit="contain"
+                />
+              </Box>
+              <Text mb={8}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras
+                sit amet massa non mauris malesuada bibendum ut non eros.
+                Curabitur mollis leo nisi, eget sagittis dui varius sit amet.
+                Praesent volutpat auctor magna.
+              </Text>
+              {/* Social icons with external links */}
+              <Stack direction="row" spacing={4}>
+                <Link href="#" isExternal>
+                  <IconButton icon={<FaYoutube />} colorScheme="white" />
+                </Link>
+                <Link href="#" isExternal>
+                  <IconButton icon={<FaFacebook />} colorScheme="white" />
+                </Link>
+                <Link href="#" isExternal>
+                  <IconButton icon={<FaLinkedinIn />} colorScheme="white" />
+                </Link>
+              </Stack>
+            </Flex>
+
+            {/* Right side with doctor image */}
+            <Image
+              src="https://source.unsplash.com/400x600/?doctor"
+              boxSize={{ base: "100%", md: "400px" }}
+            />
+          </Flex>
+        </Box>
       </Stack>
     </Container>
   );
